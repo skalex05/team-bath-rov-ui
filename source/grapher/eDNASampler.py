8ba5cc580ca821bd8e9a14475d8a16694778c068
skalex05 <skalex05@gmail.com>
2025-04-13 01:08:27 +0100

    Some bug fixes for the ROV interface when camera_count > 1. Added new UI for displaying eDNA and Migration Model Results within the UI

--- source/grapher/eDNASampler.py
+++ source/grapher/eDNASampler.py
@@ -2,11 +2,15 @@ import cv2
 import pytesseract
 from Bio.Align import PairwiseAligner
 import os
-import PIL
 from PIL import Image
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtWidgets import QWidget
 
-class eDNASampler:
+
+class eDNASampler(QWidget):
+    progress_update = pyqtSignal(int)
     def __init__(self):
+        super().__init__()
         self.aligner = PairwiseAligner()
         self.aligner.mode = "local"
         self.aligner.match_score = 2
@@ -42,14 +46,26 @@ class eDNASampler:
         clean_text = ''.join([c if c in "ACGT" else " " for c in text]).strip()
         return clean_text
 
-    def generate_results(self):
+    def generate_results(self, unknown_folder, species_folder):
+        self.results = []
         unknown_seqs = {}
         unknown_folder = os.path.join(os.getcwd(), "Unknown_photo")
+        unknown_files = sorted(os.listdir(unknown_folder))
         compressed_folder = os.path.join(unknown_folder, "compressed")
         os.makedirs(compressed_folder, exist_ok=True)
 
+        species_seqs = {}
+        species_folder = os.path.join(os.getcwd(), "Species")
+        species = os.listdir(species_folder)
+
+        total_steps = len(unknown_files)*2 + len(species) + 10
+        current_step = 0
+
+        self.progress_update.emit(current_step)
+
         unknown_sample_counter = 0
-        for unknown in sorted(os.listdir(unknown_folder)):
+
+        for unknown in unknown_files:
             if unknown.lower().endswith((".png", ".jpg", ".jpeg")):
                 display_name = f"unknown_sample_{unknown_sample_counter}"
                 input_path = os.path.join(unknown_folder, unknown)
@@ -63,33 +79,39 @@ class eDNASampler:
                     img_resized.save(output_path)
                 unknown_seqs[display_name] = self.read_sample(output_path)
                 unknown_sample_counter += 1
+            current_step += 1
+            self.progress_update.emit(int((current_step/total_steps) * 100))
 
-        species_seqs = {}
-        species_folder = os.path.join(os.getcwd(), "Species")
-        for species in os.listdir(species_folder):
+        for species in species:
             if species.lower().endswith((".png", ".jpg", ".jpeg")):
                 display_name = species.replace("_", " ").split(".")[0]
                 species_seqs[display_name] = self.read_sample(os.path.join(species_folder, species))
+            current_step += 1
+            self.progress_update.emit(int((current_step / total_steps) * 100))
 
         results = {}
         for unknown, unknown_seq in unknown_seqs.items():
-            scores = {species: self.aligner.score(species_seq, unknown_seq) for species, species_seq in species_seqs.items()}
+            scores = {species: self.aligner.score(species_seq, unknown_seq) for species, species_seq in
+                      species_seqs.items()}
             results[unknown] = scores
+            current_step += 1
+            self.progress_update.emit(int((current_step / total_steps) * 100))
+
+        self.progress_update.emit(int((current_step / total_steps) * 100))
 
         min_acceptance_score = 400
         for result in results:
-            #print(f"{result}:")
-            sample_index_normal = str(int(result[-1])+1)
+            sample_index_normal = str(int(result[-1]) + 1)
 
             best_match = max(results[result], key=results[result].get, default=None)
             best_score = results[result].get(best_match, 0)
 
             if best_score >= min_acceptance_score:
-                #print(f"\tMatch Found! {best_match} with score {best_score}")
-                self.results.append(f"Sample {sample_index_normal}: {best_match} with score {best_score}.")
+                self.results.append((sample_index_normal, best_score, best_match))
 
             else:
-                #print("\tNo Match Found")
-                self.results.append(f"Sample {sample_index_normal} highest: {best_score}")
-                
+                self.results.append((sample_index_normal, best_score, "N/A"))
+
+        self.progress_update.emit(100)
+
         return self.results
